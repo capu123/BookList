e3b523c03071fca7ddf1d5b477641549ffd9e99a
Saideep <saideep_chhetri@yahoo.com>
2017-02-11 23:54:58 +0000

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { HomePage } from '../pages/home/home';
 import { AngularFireModule } from 'angularfire2';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyAKco-OfSDVo4ScvPKWWKqhuAtYL__PiSU",
-    authDomain: "my-booklist-c6f9c.firebaseapp.com",
-    databaseURL: "https://my-booklist-c6f9c.firebaseio.com",
-    storageBucket: "my-booklist-c6f9c.appspot.com",
-    messagingSenderId: "400479026400"
+    apiKey: "**********************************",
+    authDomain: "**********************************",
+    databaseURL: "**********************************",
+    storageBucket: "*****************",
+    messagingSenderId: "*****************"
 };
 
 @NgModule({
